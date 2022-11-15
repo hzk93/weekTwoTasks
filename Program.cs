ecb2ab57b6b18a9fb0d69066df32d9bd4bdfba46
Hassan Zahoor Khan <hassanzk93@gmail.com>
2022-11-15 15:44:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace weekTwoTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            checkIfPrime prime = new checkIfPrime();
+            prime.isItaPrime();
+            }
+    }
+}
---
+++ checkIfPrime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weekTwoTasks
+{
+    public class checkIfPrime
+    {
+        public void isItaPrime()
+        {
+            int n, a, m = 0, primeIndicator = 0;
+            Console.Write("Enter a number to check for primality: \n");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for(a=2; a<=m; a++)
+            {
+                if(n%a == 0)
+                {
+                    Console.Write($"The Entered Number -> \'{n}\' is Not A Prime");
+                    primeIndicator = 1;
+                    break;
+                }
+            }
+            if(primeIndicator == 0)
+            {
+                Console.Write($"The Entered Number -> \'{n}\' Is A Prime");
+            }
+
+        }
+    }
+}
